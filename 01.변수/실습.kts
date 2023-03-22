6159a3d52a4245e66cac5abdbbb416f11b3728f7
Brian-Cho <75934498+Brian-Cho@users.noreply.github.com>
2023-03-22 16:51:04 +0900

    Add files via upload

---
+++ 01.변수/실습.kts
@@ -0,0 +1,20 @@
+// 주석 작성하는 방법
+// 주석 -> 사람만 보기 위해서 작성한다
+
+// 1. var 변수 선언하기
+var changeYes = 100
+//var changeYes // 선언과 동시에 초기화를 시켜줘야 한다 (property must be initialized or be abstract)
+println(changeYes)
+
+changeYes = 200
+println(changeYes)
+
+// 2. val 변수 선언하기
+val CHANGE_NO = 100
+println(CHANGE_NO)
+//CHANGE_NO = 200 // val cannot be reassigned -> value는 재구성 할 수 없다 (재할당)
+
+val SCHOOL_NAME = "대한민국 학교"
+println(SCHOOL_NAME)
+
+//Reformat Code 단축키 Ctrl + Alt + L -> 코드 모양 이쁘게 정렬해줌 (몰라도 가능)
